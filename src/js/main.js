679638ddccde00e562e4f60b4717a5b5b8e68723
clancysong <clancysong@gmail.com>
2018-05-02 19:16:46 +0800

    替换页面元素为可编辑组件

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,29 @@ const paper = new Vue({
     data: {
         data: {
             name: '你的姓名',
-            intro: '一句话介绍自己，告诉HR为什么选择你而不是别人'
+            intro: '一句话介绍自己，告诉HR为什么选择你而不是别人',
+            birthday: '出生日期',
+            city: '所在城市',
+            workingYears: '工作年限',
+            phoneNumber: '电话号码',
+            email: '联系邮箱',
+            intentionalPost: '意向岗位',
+            jobType: '职业类型',
+            intentionalCity: '意向城市',
+            entryTime: '入职时间',
+            selfAssessment: '在这里填写自我评价....',
+            educationBackground: [{
+                time: '填写时间',
+                school: '填写学校名称',
+                major: '填写专业名称',
+                description: '在这里详细描述你在校期间所学的专业，主要包括课程内容，校内任职，校内荣誉等内容...'
+            }],
+            projectExperience: [{
+                time: '填写时间',
+                school: '填写项目名称',
+                major: '填写角色名称',
+                description: '在这里添加描述....'
+            }],
         }
     },
     methods: {
@@ -18,16 +40,13 @@ const paper = new Vue({
             props: ['initData'],
             template: `
                 <div class="editable-element">
-                    <span v-if="!editing" v-on:click="edit">{{ data.content }}</span>
-                    <input type="text" v-if="editing" v-model="data.content" @blur="save">
+                    <span v-if="!editing" v-on:click="edit">{{ data.value }}</span>
+                    <input type="text" v-if="editing" v-model="data.value" @blur="save">
                 </div>    
             `,
             data() {
                 return {
-                    data: {
-                        name: this.initData.name,
-                        content: this.initData.content,
-                    },
+                    data: this.initData,
                     editing: false
                 }
             },
@@ -37,7 +56,7 @@ const paper = new Vue({
                 },
                 save() {
                     this.editing = false
-                    this.$emit('save', { [this.data.name]: this.data.content })
+                    this.$emit('save', { [this.data.key]: this.data.value })
                 }
             }
         }
